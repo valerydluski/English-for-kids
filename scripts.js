1f0434e3505deb380733de9a8959c6329e40a776
valerydluski <valerydluski@gmail.com>
2020-04-08 15:50:27 +0300

    feat: add header and hamburger-menu

---
+++ scripts.js
@@ -0,0 +1,18 @@
+console.log('hello');
+//sript for hamburger
+const hamburger = document.getElementById('hamburger');
+const navigation = document.getElementById('navigation')
+hamburger.addEventListener('click', () => {
+	{
+		if (hamburger.classList[1] == 'active') {
+      hamburger.classList.remove('active');
+      hamburger.classList.add('not-active');
+      navigation.classList.remove('navigation-active');
+		}
+		else{
+      hamburger.classList.remove('not-active');
+      hamburger.classList.add('active');
+      navigation.classList.add('navigation-active');
+		}	
+	}	
+})
